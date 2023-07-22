80df18ab5b518d96629673b5b8e150ffd17efff6
jeffreylin1 <jeffreychenlin@gmail.com>
2023-07-21 22:11:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+/*global chrome*/
 import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+import { Box, Button, Container, Grid, TextField } from "@mui/material";
+
 function App() {
+  const [prompt, setPrompt] = useState("");
+
+  async function handleSubmit() {}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box sx={{ width: "100%", mt: 4}}>
+        <Grid container>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              autoFocus
+              label="Your text"
+              variant="outlined"
+              multiline
+              rows={4}
+              margin="normal"
+              value={prompt}
+              onChange={(e)=>{
+                setPrompt(e.target.value);
+              }}
+            />
+            <Button 
+              fullWidth
+              disableElevation
+              variant="contained"
+              onClick={() =>handleSubmit()}>
+                Submit
+              </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 
